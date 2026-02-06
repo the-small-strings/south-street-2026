3cd9260b0d36dac18a5c34db8ebd25f2b59575d9
Stuart Leeks <stuart@leeks.net>
2026-02-06 13:22:07 +0000

    Comment out crash endpoint

--- src/api/src/routes/health.ts
+++ src/api/src/routes/health.ts
@@ -11,8 +11,8 @@ router.get('/', (req: Request, res: Response) => {
 });
 
 // Crash endpoint for easy testing of process restarts and state persistence
-router.post('/crash', (req: Request, res: Response) => {
-  console.log('Crash endpoint called - exiting process with non-zero code');
-  res.status(200).json({ message: 'Server shutting down...' });
-  process.exit(1);
-});
+// router.post('/crash', (req: Request, res: Response) => {
+//   console.log('Crash endpoint called - exiting process with non-zero code');
+//   res.status(200).json({ message: 'Server shutting down...' });
+//   process.exit(1);
+// });
